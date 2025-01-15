b41da5bd355d18c60c26924493cf30c9dacd7ed2
divinegupta0611 <guptadivine0611@gmail.com>
2025-01-15 08:17:07 +0530

    commit

--- src/data/statesView.ts
+++ src/data/statesView.ts
@@ -4,63 +4,63 @@ export const states = [
     sanskrit: "आन्ध्र प्रदेश",
     description: "Land of ancient temples and tech hubs",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.deccanherald.com/deccanherald%2Fimport%2Fsites%2Fdh%2Ffiles%2Farticle_images%2F2018%2F10%2F27%2Ffile72fkvr968o2ph5m120u-1540582201.jpg?w=1200&ar=40%3A21&auto=format%2Ccompress&ogImage=true&mode=crop",
   },
   {
     name: "Arunachal Pradesh",
     sanskrit: "अरुणाचल प्रदेश",
     description: "The land of dawn-lit mountains",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://lp-cms-production.imgix.net/2019-06/GettyImages-548700815_full.jpg",
   },
   {
     name: "Assam",
     sanskrit: "असम",
     description: "Gateway to the Northeast, home of one-horned rhinos",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://lp-cms-production.imgix.net/2022-03/shutterstockRF_199560017.jpg?sharp=10&vib=20&w=1200&w=600&h=400",
   },
   {
     name: "Bihar",
     sanskrit: "बिहार",
     description: "Cradle of ancient learning and Buddhism",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://s7ap1.scene7.com/is/image/incredibleindia/nalanda-stupa-heritage-nalanda-bihar-1-attr-hero?qlt=82&ts=1726741017068",
   },
   {
     name: "Chhattisgarh",
     sanskrit: "छत्तीसगढ़",
     description: "Heart of tribal India with dense forests",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://www.tatacapital.com/blog/wp-content/uploads/2024/05/may-3.jpg",
   },
   {
     name: "Goa",
     sanskrit: "गोवा",
     description: "Paradise of beaches and Portuguese heritage",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://media.istockphoto.com/id/535168027/photo/india-goa-palolem-beach.jpg?s=612x612&w=0&k=20&c=iGV1Ue0Efj87dQirWnUpZVG1dNobUjfVvMGdKHTJ7Qg=",
   },
   {
     name: "Gujarat",
     sanskrit: "गुजरात",
     description: "Land of endless heritage and enterprise",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://s7ap1.scene7.com/is/image/incredibleindia/2-pavagadh-temple-gujarat-state-hero2?qlt=82&ts=1726733709050",
   },
   {
     name: "Haryana",
     sanskrit: "हरियाणा",
     description: "Where tradition meets modernity",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://www.hry.in/wp-content/uploads/2021/11/word-image-2.jpeg",
   },
   {
     name: "Himachal Pradesh",
     sanskrit: "हिमाचल प्रदेश",
     description: "The abode of snow and adventure",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://media.istockphoto.com/id/1371289822/photo/himalayan-village-town-of-kalpa-with-kailash-mountain-snow-peaks-at-himachal-pradesh-india.jpg?s=612x612&w=0&k=20&c=ibz1ktqV34YlHk0FeSyBcoykG2IVViXNUxU2NLCGsg8=",
   },
   {
     name: "Jharkhand",
@@ -74,70 +74,71 @@ export const states = [
     sanskrit: "कर्नाटक",
     description: "Silicon valley of India with rich heritage",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://s7ap1.scene7.com/is/image/incredibleindia/2-vitthala-temple-complex-hampi-karnataka-city-hero?qlt=82&ts=1726721012585",
   },
   {
     name: "Kerala",
     sanskrit: "केरल",
     description: "God's own country with serene backwaters",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://traveldudes.com/wp-content/uploads/2020/09/Kerala_Main.jpg",
   },
   {
     name: "Madhya Pradesh",
     sanskrit: "मध्य प्रदेश",
     description: "Heart of incredible India",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://static.toiimg.com/photo/msid-106486632,width-96,height-65.cms",
   },
   {
     name: "Maharashtra",
     sanskrit: "महाराष्ट्र",
     description: "Land of warriors and dreamers",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH1OCyppUHBaI7lAiS7gphSYabFe2YXyP4Sg&s",
   },
   {
     name: "Manipur",
     sanskrit: "मणिपुर",
     description: "Jewel of the Northeast",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://www.incredible-northeastindia.com/images/manipur-head.jpg",
   },
   {
     name: "Meghalaya",
     sanskrit: "मेघालय",
     description: "Abode of clouds and living root bridges",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://backpackersunited.in/_next/image?url=https%3A%2F%2Fbpu-images-v1.s3.eu-north-1.amazonaws.com%2Fuploads%2F1725453946826_7%20sisters%20waterfall.jpeg&w=3840&q=75",
   },
   {
     name: "Mizoram",
     sanskrit: "मिजोरम",
     description: "Land of rolling hills and blue mountains",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://static.toiimg.com/photo/msid-84244923,width-96,height-65.cms",
   },
   {
     name: "Nagaland",
     sanskrit: "नागालैंड",
     description: "Land of festivals and warrior tribes",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.herzindagi.info/image/2023/Apr/places-to-visit-in-nagaland.jpg",
   },
+  // {
+  //   name: "Odisha",
+  //   sanskrit: "ओडिशा",
+  //   description: "Soul of ancient architecture and arts",
+  //   coverImage:
+  //     "https://s7ap1.scene7.com/is/image/incredibleindia/1-lingaraj-temple-bhubaneshwar-odisha-city-hero?qlt=82&ts=1726663559461",
+  // },
   {
-    name: "Odisha",
-    sanskrit: "ओडिशा",
-    description: "Soul of ancient architecture and arts",
-    coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
+
     name: "Punjab",
     sanskrit: "पंजाब",
     description: "Land of five rivers and golden fields",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://upload.wikimedia.org/wikipedia/commons/6/69/Golden_Temple%2C_Amritsar%2C_Punjab_UNAG.jpg",
   },
   {
     name: "Rajasthan",
@@ -151,48 +152,48 @@ export const states = [
     sanskrit: "सिक्किम",
     description: "Mystical land of monasteries and peaks",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://www.gosikkim.in/wp-content/uploads/2023/05/TRAVEL-TIPS-Sikkim-800x445.jpg",
   },
   {
     name: "Tamil Nadu",
     sanskrit: "तमिल नाडु",
     description: "Land of temples and classical arts",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE84wFXFw52DhvJ-A9heuzkNhziYQpeRGZ9A&s",
   },
   {
     name: "Telangana",
     sanskrit: "तेलंगाना",
     description: "Where heritage meets technology",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://www.holidify.com/images/bgImages/TELANGANA.jpg",
   },
   {
     name: "Tripura",
     sanskrit: "त्रिपुरा",
     description: "Land of royal heritage and natural beauty",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://upload.wikimedia.org/wikipedia/commons/c/c8/Ujjayanta_palace_Tripura_State_Museum_Agartala_India.jpg",
   },
   {
     name: "Uttar Pradesh",
     sanskrit: "उत्तर प्रदेश",
     description: "Heart of Indian civilization and culture",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://s7ap1.scene7.com/is/image/incredibleindia/dashashwamedh-ghat-varanasi-uttar-pradesh-city-hero?qlt=82&ts=1726649273578",
   },
   {
     name: "Uttarakhand",
     sanskrit: "उत्तराखण्ड",
     description: "Land of gods and Himalayan grandeur",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://static.toiimg.com/photo/msid-98653295,width-96,height-65.cms",
   },
   {
     name: "West Bengal",
     sanskrit: "पश्चिम बंगाल",
     description: "Where culture paints every corner",
     coverImage:
-      "https://images.unsplash.com/photo-1712516905584-37b34fc5de86?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://s7ap1.scene7.com/is/image/incredibleindia/1-howrah-bridge-kolkata-west-bengal-city-hero?qlt=82&ts=1726642884073",
   },
 ];
