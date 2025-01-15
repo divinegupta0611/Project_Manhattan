09fc2b9c618d2c76de2f77697df6c90ede3ea445
divinegupta0611 <guptadivine0611@gmail.com>
2025-01-15 08:16:08 +0530

    commit

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,21 @@
 import { StateData } from "./types";
 import { rajasthanData } from "./states/rajasthan";
-
+import {arunachalpradeshData} from "./states/arunachalpradesh"
+import {assamData} from "./states/assam"
+import {goaData} from "./states/Goa"
+import {punjabData} from "./states/punjabData"
+import {uttrakhandData} from "./states/uttrakhand"
+import {MaharashtraData} from "./states/Maharashtra"
+import {keralaData} from "./states/kerala"
+import {haryanaData} from "./states/haryana"
 export const stateDataMap: Record<string, StateData> = {
   rajasthan: rajasthanData,
+  arunachalpradesh: arunachalpradeshData,
+  assam : assamData,
+  goa : goaData,
+  punjab : punjabData,
+  uttrakhand: uttrakhandData,
+  maharashtra: MaharashtraData,
+  kerala:keralaData,
+  haryana:haryanaData,
 };
